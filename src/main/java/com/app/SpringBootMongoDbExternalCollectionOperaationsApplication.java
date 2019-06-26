e96b0a0cbe4c679219963134b29bcd999be9d610
Mahendranath Reddy <Mahendranath Reddy@DESKTOP-K0U7G4A>
2019-06-26 11:13:22 +0530

    commit

---
+++ src/main/java/com/app/SpringBootMongoDbExternalCollectionOperaationsApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDbExternalCollectionOperaationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDbExternalCollectionOperaationsApplication.class, args);
+	}
+
+}
